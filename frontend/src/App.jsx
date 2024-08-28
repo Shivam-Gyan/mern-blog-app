ac6185fbbf14caf5e21a38cece71978e47749c66
Shivam Gupta <shivamgupta191092005@gmail.com>
2024-08-28 10:33:56 +0530

    navbar completed,basic route setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components";
 
 
 const App = () => {
     return (
-        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Navbar/>} />
+            <Route path='/signin' element={<h1>signin page</h1>} />
+            <Route path='/signup' element={<h1>signup page</h1>} />
+        </Routes>
     )
 }
 
